3dc8a0df8cbf29d2a2d27b2f3520fdcfa321b37b
Kiruthika Krishnamoorthy <62352607+Kiruthika111@users.noreply.github.com>
2021-04-22 16:34:14 +0530

    Add files via upload

---
+++ build/precache-manifest.52d2fc857a2304765547e878989b8f08.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2eed5bbf4dbb1855e602e70aa7194ea9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5032d08a590920ce2a09",
+    "url": "/static/css/main.b35f6366.chunk.css"
+  },
+  {
+    "revision": "eecabe4e83f468947559",
+    "url": "/static/js/2.0b7ba955.chunk.js"
+  },
+  {
+    "revision": "cbe9d86c4ff38eb5938f6cad57bd7313",
+    "url": "/static/js/2.0b7ba955.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5032d08a590920ce2a09",
+    "url": "/static/js/main.cd6bb480.chunk.js"
+  },
+  {
+    "revision": "e5593d5421b1fc780b39",
+    "url": "/static/js/runtime-main.cb7faffb.js"
+  }
+]);
---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { useEffect, useState } from 'react';
+import './App.css';
+import CurrencyRow from './CurrencyRow'
+
+
+function App() {
+  
+  return (
+    <div>
+      <CurrencyRow/>
+    </div>
+  );
+}
+
+export default App;
